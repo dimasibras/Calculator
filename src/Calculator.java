bfd9a956dc3013baa31160900b00fbb40388fd7e
dimas <dimasibras10@gmail.com>
2023-12-06 12:51:28 +0700

    Initial Commit

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator{
+
+    public void add(int a, int b){
+        int result = a + b;
+        System.out.println(a+" + "+b+" = "+result);
+    }
+
+    public void sub(int a, int b){
+        int result = a - b;
+        System.out.println(a+" - "+b+" = "+result);
+    }
+
+    public void multiply(int a, int b){
+        int result = a * b;
+        System.out.println(a+" * "+b+" = "+result);
+    }
+
+    public void divide(int a, int b){
+        int result = a / b;
+        System.out.println(a+" / "+b+" = "+result);
+    }
+
+}
+
---
+++ src/Maths.java
@@ -0,0 +1,10 @@
+public class Maths extends Calculator {
+    public static void main(String[] args) {
+        Maths maths = new Maths();
+
+        maths.add(10, 5);
+        maths.sub(10, 5);
+        maths.multiply(10, 5);
+        maths.divide(10, 5);
+    }
+}
